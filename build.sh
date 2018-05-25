5978c0faea5fbf962ff127e26771139fb435beba
Toby Dylan Hocking <tdhock5@gmail.com>
2018-05-25 09:00:05 -0400

    remove NoteAbove for CRAN

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 cd ..
 rm -rf directlabels-release
 cp -r directlabels directlabels-release
+grep -v NoteAbove directlabels/DESCRIPTION > directlabels-release/DESCRIPTION
 PKG_TGZ=$(R CMD build directlabels-release|grep building|sed 's/.*‘//'|sed 's/’.*//')
 R CMD check --as-cran $PKG_TGZ
