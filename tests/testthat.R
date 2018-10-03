cf73a92daa66ec1a37b6cf8877df577213e8b044
Jon Olav Vik <jonovik@gmail.com>
2018-10-04 01:20:33 +0200

    Add test to demonstrate error with non-syntactic variable names.

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(directlabels)
+
+test_check("directlabels")
---
+++ tests/testthat/test_nonsyntactic.R
@@ -0,0 +1,6 @@
+context("Nonsyntactic names")
+test_that("Nonsyntactic variable names are accepted", {
+  library(ggplot2)
+  mpg2 <- within(mpg, `car class` <- class)
+  direct.label(ggplot(mpg2, aes(hwy, cty, colour=`car class`)) + geom_point())
+})
