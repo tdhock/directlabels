833af0e6b3f2206d3441f0d8dc7e4c36610d2a11
Toby Dylan Hocking <tdhock5@gmail.com>
2021-06-28 09:30:36 -0400

    new lars code

--- tests/doc/lineplot/lars.R
+++ tests/doc/lineplot/lars.R
@@ -1,17 +1,18 @@
 data(prostate)
-pros <- subset(prostate,select=-train,train==TRUE)
+pros <- subset(prostate, train==TRUE, -train)
 ycol <- which(names(pros)=="lpsa")
-x <- as.matrix(pros[-ycol])
+x <- as.matrix(pros)[, -ycol]
 y <- pros[[ycol]]
-library(lars)
-fit <- lars(x,y,type="lasso")
-beta <- scale(coef(fit),FALSE,1/fit$normx)
-arclength <- rowSums(abs(beta))
-library(reshape2)
-path <- data.frame(melt(beta),arclength)
-names(path)[1:3] <- c("step","variable","standardized.coef")
+fit <- lars::lars(x,y,type="lasso")
+beta.mat <- scale(coef(fit),FALSE,1/fit[["normx"]])
+arclength <- rowSums(abs(beta.mat))
+path <- data.frame(
+  arclength,
+  variable=colnames(beta.mat)[as.integer(col(beta.mat))],
+  standardized.coef=as.numeric(beta.mat),
+  step=as.integer(row(beta.mat)))
 library(ggplot2)
 ggplot(path,aes(arclength,standardized.coef,colour=variable))+
   geom_line(aes(group=variable))+
-  ggtitle("LASSO path for prostate cancer data calculated using the LARS")+
-  xlim(0,20)
+  ggtitle("LASSO path for prostate cancer data")+
+  xlim(-1,21)
