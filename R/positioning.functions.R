cf317fe3a8c984b900c38f5938670aa053ba5681
Anirban166 <Anirban166@users.noreply.github.com>
2021-07-07 03:08:53 +0000

    Deploying to gh-pages from @ tdhock/directlabels@8cc9cd283a4f1b4a988a1b240bd2aa0aeef30129 🚀

--- R/positioning.functions.R
+++ R/positioning.functions.R
@@ -67,10 +67,12 @@ makeContent.dlgrobtree <- function
   }
   text.name <- paste0(
     "directlabels.text.", x$name)
-  with(cm.data, grid.text(
+  tg <- with(cm.data, textGrob(
     label,x,y,hjust=hjust,vjust=vjust,rot=rot,default.units="cm",
     gp=gp,
     name=text.name))
+  sg <- unlist(attr(x$data, 'shapeGrobs'))
+  setChildren(x, gList(tg, sg))
 }
 
 ### This environment holds an integer id that will be incremented to
