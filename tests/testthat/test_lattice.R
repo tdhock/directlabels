2733b352401638fcb9392c59a0846143ad014eac
Toby Dylan Hocking <Toby.Hocking@nau.edu>
2023-08-25 23:30:22 +0100

    Suggests conditional in examples/tests

--- tests/testthat/test_lattice.R
+++ tests/testthat/test_lattice.R
@@ -1,13 +1,14 @@
 library(directlabels)
-library(lattice)
 library(testthat)
-test_that("panel.superpose.dl works", {
-  loci <- data.frame(
-    ppp=c(rbeta(8,10,10),rbeta(2,0.15,1),rbeta(2,1,0.15)),
-    type=factor(c(rep("NEU",8),rep("POS",2),rep("BAL",2))))
-  direct.label(densityplot(~ppp,loci,groups=type,n=500))
-  computed <- suppressWarnings(with(loci, panel.superpose.dl(
-    x=ppp, groups = type, subscripts=seq_along(ppp),
-    method="top.bumptwice", panel.groups="panel.densityplot")))
-  expect_identical(computed, NULL)
-})
+if(require(lattice)){
+  test_that("panel.superpose.dl works", {
+    loci <- data.frame(
+      ppp=c(rbeta(8,10,10),rbeta(2,0.15,1),rbeta(2,1,0.15)),
+      type=factor(c(rep("NEU",8),rep("POS",2),rep("BAL",2))))
+    direct.label(densityplot(~ppp,loci,groups=type,n=500))
+    computed <- suppressWarnings(with(loci, panel.superpose.dl(
+      x=ppp, groups = type, subscripts=seq_along(ppp),
+      method="top.bumptwice", panel.groups="panel.densityplot")))
+    expect_identical(computed, NULL)
+  })
+}
--- tests/testthat/test_nonsyntactic.R
+++ tests/testthat/test_nonsyntactic.R
@@ -1,10 +1,10 @@
 library(testthat)
-context("Nonsyntactic names")
-test_that("Nonsyntactic variable names are accepted", {
-  library(ggplot2)
-  mpg2 <- within(mpg, `car class` <- class)
-  gg <- ggplot(mpg2, aes(hwy, cty, colour=`car class`)) +
-    geom_point()
-  p <- directlabels::direct.label(gg)
-  expect_is(p, "ggplot")
-})
+if(require(ggplot2)){
+  test_that("Nonsyntactic variable names are accepted", {
+    mpg2 <- within(mpg, `car class` <- class)
+    gg <- ggplot(mpg2, aes(hwy, cty, colour=`car class`)) +
+      geom_point()
+    p <- directlabels::direct.label(gg)
+    expect_is(p, "ggplot")
+  })
+}
