c642e5da471ef47ae7c231844c693835bc591abd
Gauarv Chaudhary <chaudharygaurav2004@gmail.com>
2026-02-08 19:49:37 +0530

    Deprecate contourplot methods and remove from docs (fixes #50)

--- tests/testthat/test_contourplot.R
+++ tests/testthat/test_contourplot.R
@@ -12,10 +12,12 @@ test_that("contourplot methods are deprecated", {
     z = as.vector(volcano)
   )
 
+  # Use ..level.. which is the old syntax but likely safer for this test context or the directlabels package versions
   p <- ggplot(volcano3d, aes(x, y, z = z)) +
-    stat_contour(aes(colour = after_stat(level)))
+    stat_contour(aes(colour = ..level..))
 
   # Check for deprecation warning by class
-  expect_warning(direct.label(p, "bottom.pieces"), class = "deprecatedWarning")
-  expect_warning(direct.label(p, "top.pieces"), class = "deprecatedWarning")
+  # Note: Must print() the ggplot object to trigger the rendering where the method is called
+  expect_warning(print(direct.label(p, "bottom.pieces")), class = "deprecatedWarning")
+  expect_warning(print(direct.label(p, "top.pieces")), class = "deprecatedWarning")
 })
