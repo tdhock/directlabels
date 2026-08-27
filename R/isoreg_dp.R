e0e5b43b00c1cac90efe3b6791104cca409326e4
Toby Dylan Hocking <toby.hocking@r-project.org>
2026-08-27 14:40:48 -0400

    man pages and tests

--- R/isoreg_dp.R
+++ R/isoreg_dp.R
@@ -1,11 +1,37 @@
-isoreg_dp <- function(data_vec){
+isoreg_dp <- function
+### Dynamic programming for isotonic regression
+##alias<< isoreg_dp_interface
+(data_vec
+### vector of numeric input data
+){
   out.list <- isoreg_dp_interface(data_vec)
   with(out.list, rep(cluster_mean, cluster_size))
+### For isoreg_dp, vector of numeric output data, same size as input, but containing non-decreasing mean values closest to inputs. For isoreg_dp_interface, list of two numeric vectors: cluster_mean and cluster_size, with Inf/0 at the end for any unused elements.
 }
-aligned_labels_dp <- function(target, half.size, B.lo, B.hi){
+
+aligned_labels_dp <- function
+### Dynamic programming for aligned label positions
+(target,
+### numeric vector of ideal label positions
+  half.size,
+### numeric vector, half size of each label
+  B.lo,
+### finite numeric lower bound of plotting area
+  B.hi
+### finite numeric upper bound of plotting area
+){
   N <- length(target)
+  if(any(diff(target)<0)){
+    stop("target must be non-decreasing")
+  }
   if(length(half.size) != N){
     stop("target and half.size should have same length")
+  }
+  if(!(is.numeric(B.lo) && length(B.lo)==1 && is.finite(B.lo))){
+    stop("B.lo must be finite numeric lower bound")
+  }
+  if(!(is.numeric(B.hi) && length(B.hi)==1 && is.finite(B.hi))){
+    stop("B.hi must be finite numeric upper bound")
   }
   if(B.hi-B.lo < sum(half.size)*2){
     stop("half.size values too large for B limits")
@@ -19,4 +45,5 @@ aligned_labels_dp <- function(target, half.size, B.lo, B.hi){
     hi < diff.opt, hi,
     default=diff.opt)
   diff.bounded+lo.vec
+### numeric vector of label positions which are closest to targets, but overlapping neither the bounds nor any neighbors.
 }
--- tests/testthat/test-CRAN.R
+++ tests/testthat/test-CRAN.R
@@ -4,6 +4,14 @@ test_that("isotonic dp result increasing", {
   out <- directlabels::isoreg_dp(input)
   expect_equal(out, c(3.5, 3.5, 7))
 })
+test_that("isotonic dp interface result", {
+  input <- c(4,3,7)
+  computed <- directlabels::isoreg_dp_interface(input)
+  expected <- list(
+    cluster_size=2:0,
+    cluster_mean=c(3.5, 7, Inf))
+  expect_equal(computed, expected)
+})
 test_that("isotonic dp error for Inf", {
   input <- c(4,Inf,7)
   expect_error({
@@ -16,8 +24,8 @@ test_that("aligned labels no diff", {
   expect_equal(out, input)
 })
 test_that("aligned labels bounds", {
-  out <- directlabels::aligned_labels_dp(c(-10, -9, 9, 10), c(1,1,1,1), -10, 10)
-  expect_equal(out, c(-9, -7, 7, 9))
+  out <- directlabels::aligned_labels_dp(c(-10, -10, 9, 10), c(1,2,1,1), -10, 10)
+  expect_equal(out, c(-9, -6, 7, 9))
 })
 test_that("aligned labels error for no data", {
   expect_error({
@@ -28,4 +36,43 @@ test_that("aligned labels error for different sizes", {
   expect_error({
     directlabels::aligned_labels_dp(1:2, 1:3, -100, 100)
   }, "target and half.size should have same length")
+})
+test_that("aligned labels error for decreasing data", {
+  expect_error({
+    directlabels::aligned_labels_dp(1:0, 1:2, -100, 100)
+  }, "target must be non-decreasing")
+})
+test_that("aligned labels error for bad lower bound", {
+  expect_error({
+    directlabels::aligned_labels_dp(1:2, 1:2, -Inf, 100)
+  }, "B.lo must be finite numeric lower bound")
+  expect_error({
+    directlabels::aligned_labels_dp(1:2, 1:2, numeric(), 100)
+  }, "B.lo must be finite numeric lower bound")
+  expect_error({
+    directlabels::aligned_labels_dp(1:2, 1:2, NA_real_, 100)
+  }, "B.lo must be finite numeric lower bound")
+  expect_error({
+    directlabels::aligned_labels_dp(1:2, 1:2, 1:2, 100)
+  }, "B.lo must be finite numeric lower bound")
+  expect_error({
+    directlabels::aligned_labels_dp(1:2, 1:2, "1", 100)
+  }, "B.lo must be finite numeric lower bound")
+})
+test_that("aligned labels error for bad upper bound", {
+  expect_error({
+    directlabels::aligned_labels_dp(1:2, 1:2, -100, Inf)
+  }, "B.hi must be finite numeric upper bound")
+  expect_error({
+    directlabels::aligned_labels_dp(1:2, 1:2, -100, numeric())
+  }, "B.hi must be finite numeric upper bound")
+  expect_error({
+    directlabels::aligned_labels_dp(1:2, 1:2, -100, NA_real_)
+  }, "B.hi must be finite numeric upper bound")
+  expect_error({
+    directlabels::aligned_labels_dp(1:2, 1:2, -100, 1:2)
+  }, "B.hi must be finite numeric upper bound")
+  expect_error({
+    directlabels::aligned_labels_dp(1:2, 1:2, -100, "1")
+  }, "B.hi must be finite numeric upper bound")
 })
